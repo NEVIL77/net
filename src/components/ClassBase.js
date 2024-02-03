218805905a3b0dbdfd108de22195f272e585228e
NEVIL77 <patelnevil778@gmail.com>
2024-02-03 16:47:27 +0530

    planing to make search using openAi

---
+++ src/components/ClassBase.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class ClassName extends React-Component{
+    Constructor(props){
+        super(props)
+        class(props)
+    }
+    this.state={
+        count1 : 1 ,
+        count2 : 2 ,
+    }
+    this.setState(
+        {
+            count1 : this.state.count+1
+        }
+    )
+}
